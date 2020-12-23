c21b45d86f3064745b33f3bee4c68bed3062123a
Akshat Jain <67745418+akshatprogrammer@users.noreply.github.com>
2020-12-23 15:52:47 +0530

    Update sorting.h

--- sorting.h
+++ sorting.h
@@ -38,3 +38,18 @@ void selection_sort(int a[],int n)
 		}
 	}
 }
+void insertionsort(int *a,int n)
+{
+	int i,val,hole;
+	for(i=1;i<n;i++)
+	{
+		val = a[i];
+		hole = i;
+		while(hole>0&&a[hole-1]>val)
+		{
+			a[hole] = a[hole-1];
+			hole-=1;
+		}
+		a[hole]=val;
+	}
+}
